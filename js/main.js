dbf9cc61b9b71a847dee9f87929d56257921585a
tang-jason <jasontang227@outlook.com>
2016-01-12 21:47:41 -0800

    adding menu links

--- js/main.js
+++ js/main.js
@@ -2,16 +2,34 @@ var myApp = myApp || {};
 
 (function() {
 	myApp.navigation = {
-
+		menuLink : function () {
+			var $navSelected = $(".jt-header-wrapper nav li");
+			$navSelected.on("click", function(e) {
+				//e.preventDefault();
+				var index = $navSelected.index(this);
+				switch(index) {
+					case 0:
+						"{{ site.baseurl }}/{{ page.lang }}";
+						break;
+					case 1:
+						"{{ site.baseurl }}/{{ page.lang }}/profile.html";
+						break;
+				}
+				console.log(index);
+			});
+		}
 	}
 })();
 
 // document.ready
 $(function() {
+
 	$(".hamburger").on("click", function(e) {
 		e.preventDefault();
 		$(".jt-header-wrapper nav ul").slideToggle();
 	});
+
+	//myApp.navigation.menuLink();
 });
 
-console.log(myApp);
+//console.log(myApp.navigation.menuLink());
