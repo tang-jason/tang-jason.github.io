e70c7c0f440b619e5411046c4492784709f7a006
tang-jason <jasontang227@outlook.com>
2016-01-12 22:49:29 -0800

    added posts

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,24 +1,12 @@
-var myApp = myApp || {};
+var myApp = (function() {
 
-(function() {
-	myApp.navigation = {
-		menuLink : function () {
-			var $navSelected = $(".jt-header-wrapper nav li");
-			$navSelected.on("click", function(e) {
-				//e.preventDefault();
-				var index = $navSelected.index(this);
-				switch(index) {
-					case 0:
-						"{{ site.baseurl }}/{{ page.lang }}";
-						break;
-					case 1:
-						"{{ site.baseurl }}/{{ page.lang }}/profile.html";
-						break;
-				}
-				console.log(index);
-			});
-		}
+	// private method
+	
+
+	return {
+
 	}
+
 })();
 
 // document.ready
