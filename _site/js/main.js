7f9b8efd6a8933bca7d62b45d7d221c27e82b7b0
tang-jason <jasontang227@outlook.com>
2016-01-19 22:59:28 -0800

    put all DOM manipulation logics within namespace

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,32 +1,65 @@
 var myApp = myApp || {};
 myApp.navigation = myApp.navigation || {};
 
+
 (function() {
-	// private function
 	myApp.navigation = {
-		publicMenuToggle : function() {
-			$(window).resize(function() {
-				if (window.innerWidth >= 550) {
-					$("nav ul").show();
-					$(".hamburger").hide();
-				} else {
-					$(".hamburger").show();
-					$("nav ul").hide();
-				}
+		toggle : {
+			menuToggle : function() {
+				$(window).resize(function() {
+					if (window.innerWidth >= 550) {
+						$("nav ul").show();
+						$(".hamburger").hide();
+					} else {
+						$(".hamburger").show();
+						$("nav ul").hide();
+					}
+				});
+			},
+			hamburgerToggle : function() {
+				$(".hamburger").on("click", function(e) {
+					e.preventDefault();
+					$(".jt-header-wrapper nav ul").slideToggle();
+				});
+			}
+		},
+		linkHover : function() {
+			$("a").hover(function() { // inFunction : mouse over
+				$(this).css("color", "#ce2c38");
+			}, function() { // outFunction : mouse out
+				$(this).css("color", "");
 			});
+			$(".btn").css("border-radius", "0px	")
+			$(".btn-primary").hover(function() {
+				$(this).css({
+					"background-color" : "#ce2c38",
+					"color" : "#ffffff"
+				});
+			}, function() {
+				$(this).css({
+					"background-color" : "",
+					"color" : ""
+				});
+			});
+		},
+		misc : function() {
+			$(".year").text(new Date().getFullYear());
 		}
 	} 
 })();
 
+var init = function() {
+	// cached reference
+	var appNav = myApp.navigation
+	var appToggle = myApp.navigation.toggle;
+	
+	appToggle.hamburgerToggle()
+	appToggle.menuToggle();
 
+	appNav.linkHover();
+	appNav.misc();
+}
 
 $(function() {
-	myApp.navigation.publicMenuToggle();
-
-	$(".year").text(new Date().getFullYear());
-
-	$(".hamburger").on("click", function(e) {
-		e.preventDefault();
-		$(".jt-header-wrapper nav ul").slideToggle();
-	});
+	init();
 });
